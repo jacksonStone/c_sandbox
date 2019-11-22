feb5c5be909bb9268851893b66514e9330262dd5
Jackson Stone <jacksonastone@gmail.com>
2019-11-22 08:03:30 -0500

    checkpoint

--- src/dynamic_list_v2/hash_map.c
+++ src/dynamic_list_v2/hash_map.c
@@ -14,9 +14,8 @@ struct {
     int entry_count;
 } typedef hash_map;
 
-
-
-#define STARTING_BUCKET_COUNT 1
+#define STARTING_BUCKET_COUNT 4
+#define STARTING_BUCKET_SIZE 1
 
 #define make_hash_map(type) _make_hash_map(sizeof(type))
 
@@ -26,7 +25,7 @@ hash_map _make_hash_map(size_t sizeOfEntry) {
     map.entry_keys = make_list(char *);
     map.buckets = make_list_with_starting_length(void *, STARTING_BUCKET_COUNT);
     for(int i = 0; i < STARTING_BUCKET_COUNT; i++) {
-        map.buckets = append_to_list(map.buckets, make_list_with_starting_length(hash_entry, 1));
+        map.buckets = append_to_list(map.buckets, make_list_with_starting_length(hash_entry, STARTING_BUCKET_SIZE));
     }
     map.bucket_count = STARTING_BUCKET_COUNT;
     return map;
@@ -44,15 +43,10 @@ void _add_hash_entry(hash_map* map, char * key, void * content) {
 
     assert(list_entry_count(map->buckets) == map->bucket_count);
     
-    for(int i = 0; i < list_entry_count(map->buckets); i++) {
-        assert(list_already_freed(map->buckets[i]) == 0);
-    }
-
     int bucket_index = hash_string(key, map->bucket_count);
     hash_entry * bucket = map->buckets[bucket_index];
     for(int i = 0; i < list_entry_count(bucket); i++) {
         if(strcmp(bucket[i].key, key) == 0) {
-            printf("matching key: %s!\n", key);
             bucket[i].content = content;
             return;
         }
@@ -67,9 +61,8 @@ void _add_hash_entry(hash_map* map, char * key, void * content) {
     map->entry_count++;
     map->entry_keys = append_to_list(map->entry_keys, key);
     map->buckets[bucket_index] = append_to_list(bucket, new_entry);
-    printf("bucket index %d\n", bucket_index);
 
-    if(map->entry_count > (map->bucket_count * 3)) {
+    if(map->entry_count > (map->bucket_count * 4)) {
         //Rehash everything
         //Grab a reference to everything
         int entry_count = map->entry_count;
@@ -77,7 +70,6 @@ void _add_hash_entry(hash_map* map, char * key, void * content) {
         int entry_index = 0;
         for(int i = 0; i < list_entry_count(map->buckets); i++) {
             hash_entry * cur_bucket = map->buckets[i];
-            assert(list_already_freed(map->buckets[i]) == 0);
             for(int j = 0; j < list_entry_count(cur_bucket); j++) {
                 hash_entry entry_copy;
                 entry_copy.key = cur_bucket[j].key;
@@ -92,12 +84,11 @@ void _add_hash_entry(hash_map* map, char * key, void * content) {
         map->entry_keys = make_list_with_starting_length(char *, entry_count * 2);
 
         //Create new buckets
-        assert(!list_already_freed(map->buckets));
         free_list(map->buckets);
         map->bucket_count *= 2;
         map->buckets = make_list_with_starting_length(void *, map->bucket_count);
         for(int i = 0; i < map->bucket_count; i++) {
-            map->buckets = append_to_list(map->buckets, make_list_with_starting_length(hash_entry, 1));
+            map->buckets = append_to_list(map->buckets, make_list_with_starting_length(hash_entry, STARTING_BUCKET_SIZE));
         }
         map->entry_count = 0;
 
@@ -116,24 +107,3 @@ void * get_hash_entry(hash_map map, char * key) {
     }
     return NULL;
 }
-
-//TODO REMOVE
-#include <stdio.h>
-int main() {
-    hash_map map = make_hash_map(char *);
-    printf("map.bucket_count = %d\n", map.bucket_count);
-    printf("map.entry_count = %d\n", map.entry_count);
-    int count = 10000;
-    char keys[count][6];
-    char contents[count][6];
-    for(int i = 0; i < count; i++) {
-        sprintf(keys[i], "%dfoo%d", i, i);
-        sprintf(contents[i], "%dHello%d", i, i);
-        // printf("Key: %s, content: %s\n", keys[i], contents[i]);
-        add_hash_entry(map, keys[i], contents[i]);
-    }
-    printf("map.bucket_count = %d\n", map.bucket_count);
-    printf("map.entry_count = %d\n", map.entry_count);
-    return 0;
-}
-
--- src/dynamic_list_v2/main_c.c
+++ src/dynamic_list_v2/main_c.c
@@ -1,22 +1,29 @@
-#include <stdio.h>
 #include "./hash_map.c"
-
+#include <stdio.h>
 int main() {
-    // int * int_list = make_list(int);
-    // append_to_list(int_list, 42);
-    // printf("int_list[0] = %d\n", int_list[0]);
-    // free_list(int_list);
     hash_map map = make_hash_map(char *);
     printf("map.bucket_count = %d\n", map.bucket_count);
     printf("map.entry_count = %d\n", map.entry_count);
-    for(int i = 0; i < 100; i++) {
-        char key[50]; 
-        char content[50];
-        sprintf(key, "foo%d", i);
-        sprintf(content, "Hello%d", i);
-        add_hash_entry(map, key, content);
+    int count = 10000;
+    char keys[count][20];
+    char contents[count][20];
+    for(int i = 0; i < count; i++) {
+        sprintf(keys[i], "%dfoo%d", i, i);
+        sprintf(contents[i], "%dHello%d", i, i);
+        // printf("Key: %s, content: %s\n", keys[i], contents[i]);
+        add_hash_entry(map, keys[i], contents[i]);
+        add_hash_entry(map, keys[i/2], contents[i/2]);
+    }
+    for(int i = 0; i < count; i++) {
+        assert(strcmp(get_hash_entry(map, keys[i]), contents[i]) == 0);
     }
     printf("map.bucket_count = %d\n", map.bucket_count);
     printf("map.entry_count = %d\n", map.entry_count);
+    char ** char_list = make_list(char *);
+    for(int i = 0; i < count; i++) {
+        char_list = append_to_list(char_list, keys[i]);
+    }
+    printf("char_list[count - 1] = %s\n", char_list[count - 1]);
+
     return 0;
 }
