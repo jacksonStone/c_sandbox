308cc4691d9760959430fc9573d3d638e26240ee
Jackson Stone <jacksonastone@gmail.com>
2019-12-30 22:26:05 -0500

    Add hashmap with mandetory allocator

--- src/dynamic_structures_2.0/main_c.c
+++ src/dynamic_structures_2.0/main_c.c
@@ -9,7 +9,7 @@
 
 typedef struct {
     char * key;
-    int key_len;
+    size_t key_len;
     void * value;
 } hashmap_value;
 typedef struct {
@@ -21,13 +21,11 @@ typedef struct {
     int entry_count;
 } hashmap;
 
-unsigned int hash_string(char * key, size_t len, unsigned int range) {
+unsigned int hash_string(char * key, size_t len, int range) {
     unsigned int hash = 0;
-    while(len--) {
-        hash = (hash << 7) + (hash >> 25) + *key;
-        key++;
-    }
-    return hash % range;
+    while (len--)
+        hash = (hash << 7) + (hash >> 25) + *key++;
+    return (hash + (hash >> 16))%range;
 }
 
 hashmap make_hashmap(growing_heap * gh){
@@ -36,19 +34,44 @@ hashmap make_hashmap(growing_heap * gh){
     };
     return hm;
 }
+void add_to_hashmap(hashmap * hm, char* key, size_t key_len, void* entry);
+//LEAKS ABOUND
+void resize_hashmap(hashmap* hm, int new_size) {
+    hashmap_bucket* old_buckets = hm->buckets;
+    int entry_count = hm->entry_count;
+    //LEAK
+    hm->buckets = make_list_with_allocator_with_size(hashmap_bucket, new_size, hm->gh);
+    for(int i = 0; i < new_size; i++) {
+        hashmap_bucket bucket = {};
+        bucket.values = make_list_with_allocator_with_size(hashmap_value, 2, hm->gh);
+        hm->buckets = append_to_list(hm->buckets, bucket);
+    }
+    int old_bucket_count = get_list_len(old_buckets);
+    hm->entry_count = 0;
+    for(int i = 0; i < old_bucket_count; i++) {
+        hashmap_value* bucket_values = old_buckets[i].values;
+        int value_count = get_list_len(bucket_values);
+        for(int j = 0; j < value_count; j++) {
+            hashmap_value value = bucket_values[j];
+            add_to_hashmap(hm, value.key, value.key_len, value.value);
+        }
 
+    }
+}
+//LEAKS SO MANY
 void add_to_hashmap(hashmap * hm, char* key, size_t key_len, void* entry) {
-    if(hm->entry_count == 0) {
+    if(hm->entry_count == 0 && !(hm->buckets)) {
         //Allot everything
-        int bucket_count = 4;
-        hm->buckets = make_list_with_allocator_with_size(hashmap_bucket, bucket_count, hm->gh);
+        int new_bucket_count = 4;
+        hm->buckets = make_list_with_allocator_with_size(hashmap_bucket, new_bucket_count, hm->gh);
         for(int i = 0; i < 4; i++) {
             hashmap_bucket bucket = {};
             bucket.values = make_list_with_allocator_with_size(hashmap_value, 2, hm->gh);
             hm->buckets = append_to_list(hm->buckets, bucket);
+
         }
 
-        unsigned int bucket_index = hash_string(key, key_len, bucket_count);
+        unsigned int bucket_index = hash_string(key, key_len, new_bucket_count);
         hashmap_bucket* target_bucket = (hm->buckets + bucket_index);
         hashmap_value hv = {
             key, 
@@ -60,9 +83,9 @@ void add_to_hashmap(hashmap * hm, char* key, size_t key_len, void* entry) {
         return;
     }
     int bucket_count = get_list_len(hm->buckets);
-    if(hm->entry_count > bucket_count * 8) {
-        //Resize
-        assert(0);
+    if(hm->entry_count >= bucket_count * 4) {
+        bucket_count *= 2;
+        resize_hashmap(hm, bucket_count);
     }
 
     unsigned int bucket_index = hash_string(key, key_len, bucket_count);
@@ -76,6 +99,8 @@ void add_to_hashmap(hashmap * hm, char* key, size_t key_len, void* entry) {
         };
         //We can append - no need to loop through entries
         target_bucket->values = append_to_list(target_bucket->values, hv);
+        hm->entry_count += 1;
+        return;
     }
     for(int i = 0; i < bucket_entries; i++) {
         hashmap_value hv = target_bucket->values[i];
@@ -92,11 +117,38 @@ void add_to_hashmap(hashmap * hm, char* key, size_t key_len, void* entry) {
         entry
     };
     target_bucket->values = append_to_list(target_bucket->values, hv);
+    hm->entry_count += 1;
+}
+void* get_hashmap_entry(hashmap* hm, char* key, int len) {
+    hashmap_bucket* buckets = hm->buckets;
+    if(hm->entry_count == 0 || !buckets) {
+        return NULL;
+    }
+    int bucket_count = get_list_len(buckets);
+    unsigned int target_bucket_index = hash_string(key, len, bucket_count);
+    hashmap_value* values = (buckets + target_bucket_index)->values;
+    int values_in_bucket = get_list_len(values);
+    if (!values_in_bucket) {
+        return NULL;
+    }
+    for(int i = 0; i < values_in_bucket; i++) {
+        hashmap_value v = values[i];
+       if(v.key_len == len) {
+           if(memcmp(v.key, key, len) == 0) {
+               return v.value;
+           }
+       }
+    }
+    return NULL;
 }
 
 void add_to_hashmap_c_string(hashmap * hm, char* key, void* entry) {
     add_to_hashmap(hm, key, strlen(key), entry);
 };
+void* get_hashmap_entry_c_string(hashmap* hm, char* key) {
+    return get_hashmap_entry(hm, key, strlen(key));
+}
+
 
 
 int main() {
@@ -108,7 +160,10 @@ int main() {
     my_ints = append_to_list(my_ints, 4);
     hashmap hm = make_hashmap(&gh);
     int k = 4;
-    add_to_hashmap_c_string(&hm, "abc", &k);
+    add_to_hashmap_c_string(&hm, "zz1", &k);
+    int * res = get_hashmap_entry_c_string(&hm, "zz1");
+    printf("Just retrieved: %d\n", *res);
+    
     //Is a no-op if you pass an allocator during list creation
     free_list(my_ints);
     gh_free(&gh);
