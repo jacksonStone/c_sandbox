f19838e3d2dd2e93593f7dee7d758a5333aa0ae9
Jackson Stone <jacksonastone@gmail.com>
2019-12-31 19:00:40 -0500

    Run speed test - and have gotten to be 2x faster

--- src/json_parser/node_parser_test.js
+++ src/json_parser/node_parser_test.js
@@ -1,9 +1,11 @@
-var a = Date.now(); var p = []; 
-for(let i = 0; i < 100000; i++){ p.push(JSON.parse(`[
-    {"foo":-1234.5678, "gee":-2234.5678, "fee": {"bar":"baz"}, "bop": [123, 456, null]},
-    {"foo":-1234.5678, "gee":-2234.5678, "fee": {"baro":"baz"}, "bop": [123, 456, null]},
-    {"foo":-1234.5678, "gee":-2234.5678, "fee": {"bare":"baz"}, "bop": [123, 456, null]},
-    {"foo":-1234.5678, "gee":-2234.5678, "fee": {"bara":"baz"}, "bop": [123, 456, null]}
-]`));}
+var q = [];
+for(let i = 0; i < 100000; i++){ q.push(`[
+    {"foo${i}":-1234.5678, "gee":-2234.5678, "fee": {"bar":"baz"}, "bop": [123, 456, null]},
+    {"foo${i + 1}":-1234.5678, "gee":-2234.5678, "fee": {"baro":"baz"}, "bop": [123, 456, null]},
+    {"foo${i + 2}":-1234.5678, "gee":-2234.5678, "fee": {"bare":"baz"}, "bop": [123, 456, null]},
+    {"foo${i + 3}":-1234.5678, "gee":-2234.5678, "fee": {"bara":"baz"}, "bop": [123, 456, null]}
+]`);}
+var a = Date.now();
+for(let i = 0; i < 100000; i++){ var p = JSON.parse(q[i]); }
 
- b = Date.now(); console.log('NODE: ' + ((b - a)/100) + ' microseconds per run')
+ b = Date.now(); console.log('NODE: ' + ((b - a)/100) + ' microseconds per run');
